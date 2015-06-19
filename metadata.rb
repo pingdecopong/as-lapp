f0e23f34171501b5225056ae8eee6c5856fcff93
fhirashima <fhirashima@arte-sys.jp>
2015-06-19 13:50:50 +0900

    debugフラグ廃止

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ source_url       'https://github.com/pingdecopong/as-lapp'
 license          'MIT'
 description      'Installs/Configures as-lapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
 
 depends 'as-apache2'
 depends 'as-php'
